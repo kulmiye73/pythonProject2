c9d1b45bbd183f28b73a7b43d9e836a97175c37b
kulmiye73 <kulmiye73@student.rtc.edu>
2021-03-20 23:42:03 -0700

    paramiko
    this code works well

---
+++ raspberry.py
@@ -0,0 +1,12 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname='10.0.0.229', username='pi',password="cna3351@")
+
+stdin, stdout, stderr = ssh.exec_command('ip addr show')
+#stdin, stdout, stderr = ssh.exec_command('sudo apt update && sudo apt upgrade -y')
+for line in stdout.read().splitlines():
+    print(line)
+
+ssh.close()
